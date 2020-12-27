7f4b207f45f6cabb3a57dbce3a4e30b6f1cd65bb
Ariel Pillemer <pillemer@gmail.com>
2020-12-27 12:25:11 +1100

    added test_maybe_place_space_before

--- test_ask.py
+++ test_ask.py
@@ -24,3 +24,33 @@ class TestAsk(unittest.TestCase):
 
 		for index, route_path in enumerate(route_paths):
 			self.assertEqual(func_names[index], ask.route_path_to_func_name(route_path))
+
+	def test_maybe_place_space_before(self):
+		token_val = '%'
+
+		parsed = [
+			'',
+			'string',
+			'string\n',
+			'string\t',
+			'string(',
+			'string ',
+			'string.',
+		]
+		
+		expected_result = [
+			' % ',
+			'string % ',
+			'string\n% ',
+			'string\t% ',
+			'string(% ',
+			'string % ',
+			'string.% ',
+		]
+		for index, value in enumerate(parsed):
+			self.assertEqual(expected_result[index], ask.maybe_place_space_before(value, token_val))
+			
+
+
+if __name__ == '__main__':
+    unittest.main()
