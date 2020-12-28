2dc3846bd3d654a7c540d9b878a598f627c65947
Edvard Busck-Nielsen <edvard1807@gmail.com>
2020-12-28 18:00:32 +0200

    Built-in & custom Decorators (#54)
    * Added support for custom decorators
    
    * Update, currently broken
    
    * Update, currently broken
    
    * Grouping functions, marking insertion now works
    
    * Update, custom decorators now work!
    
    * Refactoring, fixed an indentation bug
    
    * Removed a print statement
    
    * Update
    
    * Update
    
    * Code quality improvements
    
    * Added unit tests, bug fix

--- test_ask.py
+++ test_ask.py
@@ -24,3 +24,100 @@ class TestAsk(unittest.TestCase):
 
 		for index, route_path in enumerate(route_paths):
 			self.assertEqual(func_names[index], ask.route_path_to_func_name(route_path))
+
+	def test_tokens_grouped_by_lines(self):
+		tokens = [
+			['VAR', 'variable'],
+			['FORMAT', '\n'],
+			['VAR', 'variable'],
+			['FORMAT', '\n'],
+			['FORMAT', '\t'],
+			['VAR', 'variable'],
+			['FORMAT', '\n'],
+			['FORMAT', '\t'],
+			['FORMAT', '\t'],
+			['VAR', 'variable'],
+		]
+
+		excpected = [
+			[
+				['VAR', 'variable'],
+			],
+			[
+				['FORMAT', '\n'],
+				['VAR', 'variable'],
+			],
+			[
+				['FORMAT', '\n'],
+				['FORMAT', '\t'],
+				['VAR', 'variable'],
+			],
+			[
+				['FORMAT', '\n'],
+				['FORMAT', '\t'],
+				['FORMAT', '\t'],
+				['VAR', 'variable'],
+			],
+		]
+
+		self.assertEqual(excpected, ask.tokens_grouped_by_lines(tokens))
+
+	def test_insert_indention_group_markers(self):
+		tokens = [
+			['FUNC_DEF', 'function'],
+			['FORMAT', '\n'],
+			['FORMAT', '\t'],
+			['FUNC', 'print'],
+			['STR', 'text'],
+			['OP', ')'],
+			['FORMAT', '\n'],
+			['FORMAT', '\t'],
+			['KEYWORD', 'if'],
+			['VAR', 'var'],
+			['OP', ':'],
+			['FORMAT', '\n'],
+			['FORMAT', '\t'],
+			['FORMAT', '\t'],
+			['FUNC', 'print'],
+			['STR', 'text'],
+			['OP', ')'],
+			['FORMAT', '\n'],
+			['FORMAT', '\t'],
+			['FUNC', 'print'],
+			['STR', 'text'],
+			['OP', ')'],
+		]
+
+		expected = [
+			['GROUP', 'start'],
+			['FUNC_DEF', 'function'],
+			['GROUP', 'start'],
+			['FORMAT', '\n'],
+			['FORMAT', '\t'],
+			['FUNC', 'print'],
+			['STR', 'text'],
+			['OP', ')'],
+			['FORMAT', '\n'],
+			['FORMAT', '\t'],
+			['KEYWORD', 'if'],
+			['VAR', 'var'],
+			['OP', ':'],
+			['GROUP', 'start'],
+			['FORMAT', '\n'],
+			['FORMAT', '\t'],
+			['FORMAT', '\t'],
+			['FUNC', 'print'],
+			['STR', 'text'],
+			['OP', ')'],
+			['GROUP', 'end'],
+			['FORMAT', '\n'],
+			['FORMAT', '\t'],
+			['FUNC', 'print'],
+			['STR', 'text'],
+			['OP', ')'],
+			['GROUP', 'end'],
+			['GROUP', 'end']
+		]
+
+		self.assertEqual(expected, ask.insert_indention_group_markers(tokens))
+
