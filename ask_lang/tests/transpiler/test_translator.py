1db4b069730a5efb4148bf1a2833a132a9dd7466
Edvard Busck-Nielsen <edvard1807@gmail.com>
2021-05-28 15:29:50 +0300

    Ask 1.3.0 (#79)
    * Improved modules.
    
    * Added support for code insertion from external files with the include keyword.
    
    * Improved test coverage
    
    * Added support for the files request type

--- ask_lang/tests/transpiler/test_translator.py
+++ ask_lang/tests/transpiler/test_translator.py
@@ -1,17 +1,24 @@
 # coding=utf-8
 import unittest
 
+from ask_lang import cfg
 from ask_lang.transpiler import translator
 
 
 class TestTranspilerTranslatorInsertBasicDecoratorCodeToInsert(unittest.TestCase):
+	@classmethod
+	def setUp(cls) -> None:
+		cfg.set_defaults()
+
 	# TODO: Add advanced tests here.
-	pass
 
 
 class TestTranspilerTranslatorTranslate(unittest.TestCase):
+	@classmethod
+	def setUp(cls) -> None:
+		cfg.set_defaults()
+
 	# TODO: Add advanced tests here.
-	pass
 
 
 if __name__ == '__main__':
